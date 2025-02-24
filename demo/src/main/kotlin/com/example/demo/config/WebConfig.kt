b67da3f0b684d3b13b8e4547d9e7f02436e837fb
minguj <minguj.jin@gmail.com>
2025-02-24 17:40:35 +0900

    CORS 주소 환경변수 적용 업데이트

--- demo/src/main/kotlin/com/example/demo/config/WebConfig.kt
+++ demo/src/main/kotlin/com/example/demo/config/WebConfig.kt
@@ -1,14 +1,20 @@
 package com.example.demo.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+import io.github.cdimascio.dotenv.dotenv
+val dotenv = dotenv()
+val APP_API_URL = dotenv["APP_API_URL"]
+
 @Configuration
 class WebConfig : WebMvcConfigurer {
+
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // React 개발 서버 주소
+            .allowedOrigins(APP_API_URL) // React 개발 서버 주소
             .allowedMethods("*") // 모든 HTTP 메서드 허용
             .allowedHeaders("*") // 모든 헤더 허용
             .allowCredentials(true) // 쿠키 허용 (필요 시)
